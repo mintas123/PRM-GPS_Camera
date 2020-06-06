f6c9313784bfed3ed5c05523aeffbc06677195b6
Jakub Mroz <kumroz@gmail.com>
2020-06-06 14:11:13 +0200

    project 2 init

---
+++ app/src/main/java/pl/pjatk/s16604/project2/CameraActivity.kt
@@ -0,0 +1,11 @@
+package pl.pjatk.s16604.project2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
