3aa492dcafa8092ace9b09f9ad79502932fdf0f4
Jakub Mroz <kumroz@gmail.com>
2020-06-09 22:57:27 +0200

    fixes, location overlay on photos added

--- app/src/main/java/pl/pjatk/s16604/project2/utils/ImageSaver.kt
+++ app/src/main/java/pl/pjatk/s16604/project2/utils/ImageSaver.kt
@@ -1,7 +1,9 @@
 package pl.pjatk.s16604.project2.utils
 
+import android.graphics.*
 import android.media.Image
 import android.util.Log
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -9,17 +11,30 @@ import java.io.IOException
 
 internal class ImageSaver(
     private val image: Image,
-    private val file: File
+    private val file: File,
+    private val watermark: Bitmap
 ) : Runnable {
 
     override fun run() {
+
         val buffer = image.planes[0].buffer
         val bytes = ByteArray(buffer.remaining())
         buffer.get(bytes)
+
+        val options = BitmapFactory.Options()
+        options.inMutable = true
+        val original = BitmapFactory.decodeByteArray(bytes, 0, bytes.size, options)
+        val overlayed = overlay(original, watermark)
+        val byteArrayOutputStream = ByteArrayOutputStream()
+
+        overlayed!!.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream)
+
         var output: FileOutputStream? = null
+        val watermarkBytes = byteArrayOutputStream.toByteArray()
+
         try {
             output = FileOutputStream(file).apply {
-                write(bytes)
+                write(watermarkBytes)
             }
         } catch (e: IOException) {
             Log.e(TAG, e.toString())
@@ -36,6 +51,8 @@ internal class ImageSaver(
     }
 
     companion object {
-        private const val TAG = "IMAGE_SAVER"
+        private const val TAG = "XX_IMAGE_SAVER"
     }
+
+
 }
